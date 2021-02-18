39e0ead4ae2b70cf11984ec7479c7ee8f4530cbd
sojeong <sojeong5879@gmail.com>
2021-02-18 11:38:06 +0900

    cors 적용

--- src/main/java/com/example/SpringProject/controller/UserController.java
+++ src/main/java/com/example/SpringProject/controller/UserController.java
@@ -11,9 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@CrossOrigin(origins = "http://3.34.215.218:80")
 @RequiredArgsConstructor
 @Controller
 public class UserController {
